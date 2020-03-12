5f6b4240a281b7d57ae74f2778f96c4bb3a8a1a1
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-03-12 00:33:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  data.each do |item, thing|
+    thing.each do |key, value|
+      value.each do |t|
+        if new_data.key?(t)
+          new_data[t][item] = key 
+        else
+          new_data[t] = {item => key}
+        end
+      end
+    end
 end
