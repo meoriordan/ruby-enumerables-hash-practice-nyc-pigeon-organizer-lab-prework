e5c922b9936bfcca7f488f7d3dac6baf8077eddc
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-03-12 00:49:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  data.each do |item, thing|
+    thing.each do |key, value|
+      value.each do |t|
+        if new_data.key?(t)
+          if new_data[t][item]
+            new_data[t][item] << key.to_s
+          else
+            new_data[t][item] = [key.to_s]
+          end
+        else
+          new_data[t] = {item => [key.to_s]}
+        end
+      end
+    end
+  end
+  return new_data
 end
